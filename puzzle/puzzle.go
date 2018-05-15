f9486584b8ddba8649e8bc4776aea6b464f26b0f
Mathieu Moullec <mathieumoullec@MBP-de-Mathieu.com>
2018-05-16 01:59:32 +0200

    done answer puzzle

--- puzzle/puzzle.go
+++ puzzle/puzzle.go
@@ -118,7 +118,7 @@ func (p *Puzzle) PrintPuzzle() {
 	u := p.Case
 	for y := 0; y < p.Size; y++ {
 		for x := 0; x < p.Size; x++ {
-			if u[y+x*p.Size] == 0 {
+			if u[x+y*p.Size] == 0 {
 				color.New(color.FgRed).Printf("|%*d| ", len(strconv.Itoa(p.Size*p.Size))+1, u[x+y*p.Size])
 			} else {
 				fmt.Printf("|%*d| ", len(strconv.Itoa(p.Size*p.Size))+1, u[x+y*p.Size])
