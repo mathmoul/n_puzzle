eebb24462f0c0779d22e40bcd5f005c99e85473b
Mathieu Moullec <mathieumoullec@MBP-de-Mathieu.com>
2018-06-10 19:20:02 +0200

    inversions

--- npuzzle/inversions.go
+++ npuzzle/inversions.go
@@ -1,5 +1,28 @@
 package npuzzle
 
-func (p *Puzzle) Inversions() (i int) {
+import "fmt"
 
+func (p *Puzzle) Inversions() (inversions int) {
+	for i := range p.Board {
+		inversions += inversion(p.Board, i)
+	}
+	return
+}
+
+func inversion(b Board, i int) (inversions int) {
+	if b[i] == 0 {
+		return 0
+	}
+	slice := b[i:]
+	fmt.Println(slice)
+	n := b[i]
+	for r := range slice {
+		if slice[r] == 0 {
+			continue
+		}
+		if n > slice[r] {
+			inversions += 1
+		}
+	}
+	return inversions
 }
--- solver/solvability.go
+++ solver/solvability.go
@@ -3,6 +3,7 @@ package solver
 import "fmt"
 
 func (a *Astar) CheckSolvability() bool {
+	a.Puzzle.PrintPuzzle()
 	i := a.Puzzle.Inversions()
 	j := a.Goal.Inversions()
 	fmt.Println(i, j)
