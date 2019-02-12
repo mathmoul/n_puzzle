0652e6c6a234a399983506e0a4d5ac3b8c6e2590
Mathieu MOULLEC <mmoullec@e1r9p13.42.fr>
2019-02-12 10:28:57 +0100

    removed echo and die statements, lolz.

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/mathmoul/n_puzzle
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/google/pprof v0.0.0-20190208070709-b421f19a5c07 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/mattn/go-colorable v0.1.0 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/theodesp/go-heaps v0.0.0-20181017084147-cb91ced45087
+	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
+	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67 // indirect
+	golang.org/x/sys v0.0.0-20190209173611-3b5209105503 // indirect
+)
--- main.go
+++ main.go
@@ -6,7 +6,7 @@
 /*   By: mmoullec <mmoullec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 17:01:00 by mmoullec          #+#    #+#             */
-/*   Updated: 2019/01/15 17:01:05 by mmoullec         ###   ########.fr       */
+/*   Updated: 2019/02/12 10:28:10 by mmoullec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,26 +55,40 @@ var t = [2]bool{
 
 var global Flags
 
+func computeSolv(f *bool, solv, unsolv bool) (err error) {
+	var r *big.Int
+	if solv {
+		*f = true
+		return nil
+	} else if unsolv {
+		*f = false
+		return nil
+	} else {
+		r, err = rand.Int(rand.Reader, big.NewInt(int64(len(t))))
+		if err != nil {
+			return err
+		}
+		*f = t[r.Int64()]
+	}
+	return nil
+}
+
 // Parse func
 func Parse() (f Flags, err error) {
-	var r *big.Int
+
 	var unsolv bool
+	var solv bool
 	flag.IntVar(&f.Size, "size", 3, "Size of the puzzle's side. Must be > 3.")
-	//flag.BoolVar(&f.Solvable, "s", true, "Forces generation of a solvable puzzle. Overrides -u.")
-	flag.BoolVar(&unsolv, "u", false, "Forces generation of an unsolvable puzzle.\n(default: random solvable or unsolvable puzzle)")
+	flag.BoolVar(&solv, "solvable", false, "Forces generation of a solvable puzzle. Overrides -u.")
+	flag.BoolVar(&unsolv, "unsolvable", false, "Forces generation of an unsolvable puzzle.\n(default: random solvable or unsolvable puzzle)")
 	flag.UintVar(&f.Iterations, "iterations", 10000, "Number of iterations.")
 	flag.UintVar(&f.Heuristic, "heu", 1,
 		"Forces heuristic, must be between 1 to 3\n\t1 = mahnattan \n\t2 = linear \n\t3 = missplaced \n")
 	flag.UintVar(&f.Cost, "c", 2, "Choose cost, must be between 1 to 3\n\t1 = Greedy Search (Only Heuristic) (faster)\n\t2 = Astar (average)\n\t3 = Uniform search (slower)\n")
 	flag.Parse()
 	f.Args = flag.Args()
-	f.Solvable = !unsolv
-	if f.Solvable {
-		r, err = rand.Int(rand.Reader, big.NewInt(int64(len(t))))
-		if err != nil {
-			return
-		}
-		f.Solvable = t[r.Int64()]
+	if err = computeSolv(&f.Solvable, solv, unsolv); err != nil {
+		return
 	}
 	if f.Heuristic < 1 || f.Heuristic > 3 {
 		log.Fatal("Wrong heuristic")
